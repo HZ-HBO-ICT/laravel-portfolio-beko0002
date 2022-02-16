b9138bbff6dd026530c11932d70a14efab8da741
beko0002 <beko0002@hz.nl>
2022-02-16 20:48:28 +0100

    Finished week 1

--- app/Models/Assignment.php
+++ app/Models/Assignment.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Faq extends Model
+class Assignment extends Model
 {
     use HasFactory;
+    public function complete(){
+        $this->completed = true;
+        $this->save();
+
+    }
 }
---
+++ app/Models/Grade.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Grade extends Model
+{
+    use HasFactory;
+
+    public function addResult($newResult){
+        if ($newResult <= $this->best_grade) {
+            $this->best_grade = $this->best_grade;
+            $this->save();
+        } else {
+            $this->best_grade = $newResult;
+            $this->save();
+        }
+
+        if ($this->best_grade >= $this->lowest_passing_grade) {
+            $this->passed_at = now();
+            $this->save();
+        }
+    }
+}
